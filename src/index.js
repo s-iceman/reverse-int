85145db11a39040e6903517f3c2a1484bb899f72
s_iceman <s_iceman@protonmail.com>
2022-01-07 22:22:28 +0300

    feat: implement function to reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = Math.abs(n);
+  let result = 0;
+  while (n > 0){
+    result = result * 10 + n % 10;
+    n = Math.floor(n / 10);
+  }
+  return result;
 }
